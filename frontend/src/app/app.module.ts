eae89d22ae2f371a07c5835f353039dae80c024f
leoportogtr86 <leoporto_studio@hotmail.com>
2021-02-22 14:18:07 -0300

    organizando as importacoes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,28 +1,29 @@
+import {HttpClientModule} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
-import { FooterComponent } from './components/template/footer/footer.component';
-import { NavComponent } from './components/template/nav/nav.component';
+
 import {MatToolbarModule} from '@angular/material/toolbar'
 import {MatSidenavModule} from '@angular/material/sidenav'
 import {MatListModule} from '@angular/material/list';
 import {MatInputModule} from '@angular/material/input'
-import { HomeComponent } from './views/home/home.component'
 import {MatCardModule} from '@angular/material/card';
-import { ProdutosComponent } from './views/produtos/produtos.component';
 import {MatButtonModule} from '@angular/material/button';
-import { CheckoutComponent } from './views/checkout/checkout.component'
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatSelectModule} from '@angular/material/select';
-import { ProductComponent } from './components/product/product.component';
+
+import { CheckoutComponent } from './views/checkout/checkout.component'
 import {ProductService} from '../app/services/product.service';
-import {HttpClientModule} from '@angular/common/http';
+import { ProductComponent } from './components/product/product.component';
 import { CardFormComponent } from './components/card-form/card-form.component';
 import {CardProductComponent} from '../app/components/card-product/card-product.component'
+import { ProdutosComponent } from './views/produtos/produtos.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component'
+import { HeaderComponent } from './components/template/header/header.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 
 
